20be1e65b9e55abcacdc4779886e24d347facfbc
syedhassani <syedmhassani12@gmail.com>
2024-12-24 11:14:48 +1100

    home work

---
+++ main.py
@@ -0,0 +1,8 @@
+# for i in range(10,60,3)
+#     print(i)
+# string=syedmuhammadalihassani
+# for i in string
+#       print(i)
+num=int(input(enter number to print tables))
+for i in range(1,20)
+    print(num,x,i,=,inum)
